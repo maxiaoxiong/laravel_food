c1f2a732298e455be1d8f18dbf61d102e9cc7d29
xiongzai <18535476501@163.com>
2016-06-02 16:16:07 +0800

    fix some bug

--- app/Api/Transformers/WindowDishesTransformer.php
+++ app/Api/Transformers/WindowDishesTransformer.php
@@ -39,7 +39,7 @@ class WindowDishesTransformer extends TransformerAbstract
             'id' => $dish['id'],
             'name' => $dish['dish_name'],
             'img_url' => $dish['dish_img'],
-            'price' => (int)$dish['dish_price'],
+            'price' => $dish['dish_price'],
             'sales' => $sales,
             'delivery_time' => $dish['delivery_time'],
             'range' => $average
