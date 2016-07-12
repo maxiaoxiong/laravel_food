bdb30d3943dd60bc4d16fec7180968c9c2d56148
maxiaoxiong <18535476501@163.com>
2016-07-12 10:23:19 +0800

    fix cropimage

--- resources/views/layouts/partials/htmlheader.blade.php
+++ resources/views/layouts/partials/htmlheader.blade.php
@@ -3,6 +3,7 @@
     <title> 小胖带饭 @yield('htmlheader_title', 'Your title here') </title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <link rel="stylesheet" href="{{ asset('/css/all.css') }}">
+    <link rel="stylesheet" href="{{ asset('/plugins/croppic/croppic.css') }}">
     <link href="//cdn.bootcss.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
     <link href="//cdn.bootcss.com/ionicons/2.0.1/css/ionicons.min.css" rel="stylesheet">
     <script src="{{ asset('/js/all.js') }}"></script>
